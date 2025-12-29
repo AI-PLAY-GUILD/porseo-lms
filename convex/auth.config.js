e27b616866c3c3e64a538e80cfa13abf9f98ad20
Shun <shunlokbne72one@icloud.com>
2025-12-29 10:47:53 +0900

    feat: Configure Clerk for production environment

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://choice-sturgeon-95.clerk.accounts.dev",
+            domain: process.env.CLERK_ISSUER_URL,
             applicationID: "convex",
         },
     ],
